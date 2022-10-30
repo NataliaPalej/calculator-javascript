c329c7e131bb60e78af9a44f87d176013e7887a8
Natalia Palej <palejnatalia@gmail.com>
2022-10-30 16:39:18 +0000

    Implementing JavaScript for calculator

--- jscripts/myScript.js
+++ jscripts/myScript.js
@@ -1,2 +1,30 @@
+"use strict";
 const $ = (selector) => document.querySelector(selector);
 
+let input = $("#input");
+
+function clearResult () {
+    $("#input").innerHTML = " ";
+    
+}
+
+function appendNumber (number){
+
+}
+
+function operation(operation){
+
+}
+
+function result(){
+    
+}
+
+function displayResult () {
+
+}
+
+window.addEventListener("load", () => {
+	$("#calculate").addEventListener("click", displayResult);
+    $("#clear").addEventListener("click", clearResult);
+});
